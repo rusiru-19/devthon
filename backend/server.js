69a302e76b856ff6aa2f5c6d78132edb07aa5a0c
Rusiru Thamara Kaluarachchi <122989375+rusiru-19@users.noreply.github.com>
2026-02-06 02:34:09 +0530

    fix: update Firebase service account configuration to use environment variables

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,15 @@ const io = new Server(server, {
 const upload = multer({ storage: multer.memoryStorage() });
 const PORT = 3000;
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // fix newlines
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+};
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
